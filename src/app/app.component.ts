6c2b2a5cdec50cf18d59382268f2aa1ae504405b
Jean Gomes <jeangomes1567@gmail.com>
2024-11-07 18:01:23 -0300

    feat: Organization of Structures in component html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
@@ -5,10 +6,16 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule],
+  imports: [RouterOutlet, MatSidenavModule, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
+
+  selectedButton: string = 'expenses';
+
+  selectButton(button: string) {
+    this.selectedButton = button;
+  }
 }
